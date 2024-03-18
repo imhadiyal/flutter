f038b183d3638d8d69831ed77ef920342014cfbc
Darshan Hadiyal <146731392+imhadiyal@users.noreply.github.com>
2024-03-18 23:51:24 +0530

    2.dart

--- pr-3/2.dart
+++ pr-3/2.dart
@@ -1,50 +1,136 @@
-/*
-Create multiple customers through array of object.
-When each customer is entered (instantiated) note their cust_id, cust_name, cust_contact
-Create a menu driven system which gives the customer verities of categories and their products.
-Whenever user selects any product simulate the feature to add product in cust_cart which will contain 4 parameters for each product pro_id, pro_name, pro_qty, pro_price.
-Apply billing and discount criteria's on the final amount after calculating each customer's number of product's price.
-                                 10% disc. on 500 - 1500
-                                 20% disc. on 1500 - 3500
-                                 25% disc. on 3500 - 6500
-                                 30% disc. for above all.
-add searching functionalities by cust_id which gives cust_id, cust_name, cust_contact, list of products, their price ang quantity and include final amount and discount amount.
-*/
+import 'dart:ffi';
 import 'dart:io';
 
-class Product {
-  late int cust_id;
-  late String cust_name;
-  late String cust_contact;
-  Product(
-      {required this.cust_id,
-      required this.cust_name,
-      required this.cust_contact}) {
-    stdout.write("Enter a id: ");
-    cust_id = int.parse(stdin.readLineSync() ?? "0");
-    stdout.write("Enter a Name: ");
-    cust_name = stdin.readLineSync() ?? "0";
-    stdout.write("Enter a contact: ");
-    cust_contact = stdin.readLineSync() ?? "0";
-  }
-  void iteam() {
-    print("1.grocery");
-    print("1.mobile");
-    print("1.fashion");
-    print("2.Electronics");
-    print("1.Home&Furnicher");
-   
+class customer {
+  int? id;
+  String? name;
+  int? contect;
+  int? cart;
+  int? pid;
+  String? pro_name;
+  int? price;
+  int A = 0;
+  int quantity = 0;
+  int? aumount;
+
+  setCustomerdata() {
+    print("\t\t|===========================|");
+    print("\t\t|    🛒 CUSTOMER DATA 🛒    |");
+    print("\t\t|===========================|");
+    stdout.write("\n\n* ENTER YOUR ID = ");
+    id = int.parse(stdin.readLineSync() ?? "0");
+    stdout.write("* ENTER YOUR NAME = ");
+    name = stdin.readLineSync() ?? "name";
+    stdout.write("* ENTER YOUR CONTECT NUMBER = ");
+    contect = int.parse(stdin.readLineSync() ?? "0");
   }
 
-  getdata() {
-    print("id:$cust_id");
-    print("name:$cust_name");
-    print("contact:$cust_contact");
+  cart_product() {
+    do {
+      stdout.write("1.[TV 📺]\n");
+      stdout.write("2.[SCHOOL BAG 🎒]\n");
+      stdout.write("3.[HADEPHONE 🎧]\n");
+      stdout.write("4.[COUMPUTER 🖥️]\n");
+      stdout.write("5.[WATCH ⌚]\n");
+      stdout.write("6.[MOBILE 📱]\n");
+      stdout.write("7.[BILL ]\n");
+      stdout.write("0.[EXIT ❌]\n");
+
+      stdout.write("* ENTER CHOOSE ONE = ");
+      cart = int.parse(stdin.readLineSync() ?? "0");
+      switch (cart) {
+        case 1:
+          A = 45000;
+          print("\t\t|===========================|");
+          print("\t\t|          📺 TV 📺         |");
+          print("\t\t|===========================|");
+          print("* SERIAL NO. = 100");
+          print("* PRODUCT NAME = TV");
+          print("* PRODUCT PRICE =  $A ");
+          stdout.write("* ENTER QUANTITY = ");
+          quantity = int.parse(stdin.readLineSync() ?? "0");
+          break;
+        case 2:
+          A = 3000;
+          print("\t\t|===========================|");
+          print("\t\t|     🎒 SCHOOL BAG 🎒      |");
+          print("\t\t|===========================|");
+          print("* SERIAL NO. = 200");
+          print("* PRODUCT NAME = SCHOOL BAG ");
+          print("* PRODUCT PRICE =  $A ");
+          stdout.write("* ENTER QUANTITY = ");
+          quantity = int.parse(stdin.readLineSync() ?? "0");
+          break;
+        case 3:
+          A = 25000;
+          print("\t\t|===========================|");
+          print("\t\t|      🎧 HADEPHONE 🎧      |");
+          print("\t\t|===========================|");
+          print("* SERIAL NO. = 300");
+          print("* PRODUCT NAME = HADEPHONE ");
+          print("* PRODUCT PRICE =  $A ");
+          stdout.write("* ENTER QUANTITY = ");
+          quantity = int.parse(stdin.readLineSync() ?? "0");
+          break;
+        case 4:
+          A = 60000;
+          print("\t\t|===========================|");
+          print("\t\t|      🖥️ COUMPUTER 🖥️        |");
+          print("\t\t|===========================|");
+          print("* SERIAL NO. = 400");
+          print("* PRODUCT NAME = COUMPUTER");
+          print("* PRODUCT PRICE =  $A ");
+          stdout.write("* ENTER QUANTITY = ");
+          quantity = int.parse(stdin.readLineSync() ?? "0");
+          break;
+
+        case 5:
+          A = 15000;
+          print("\t\t|===========================|");
+          print("\t\t|        ⌚ WATCH ⌚       |");
+          print("\t\t|===========================|");
+          print("* SERIAL NO. = 500");
+          print("* PRODUCT NAME = WATCH");
+          print("* PRODUCT PRICE =  $A ");
+          stdout.write("* ENTER QUANTITY = ");
+          quantity = int.parse(stdin.readLineSync() ?? "0");
+          break;
+        case 6:
+          A = 150000;
+          print("\t\t|===========================|");
+          print("\t\t|       📱 MOBILE 📱       |");
+          print("\t\t|===========================|");
+          print("* SERIAL NO. = 600");
+          print("* PRODUCT NAME = MOBILE");
+          print("* PRODUCT PRICE =  $A ");
+          stdout.write("* ENTER QUANTITY = ");
+          quantity = int.parse(stdin.readLineSync() ?? "0");
+          break;
+
+        case 7:
+          print("\t\t|===========================|");
+          print("\t\t|       ..YOUR BILL..       |");
+          print("\t\t|===========================|");
+          print("ID =        $id");
+          print("NAME =      $name");
+          print("CONTECT =    $contect");
+          print("AMOUNT =    ${A * quantity}");
+          break;
+
+        case 0:
+          print("\n\n\t\t...EXIT ❌...\n\n");
+          break;
+
+        default:
+          print("\n\n\t\t..❗❗ INVALID INPUT ❗❗..\n\n");
+      }
+    } while (cart != 0);
   }
 }
 
 void main(List<String> args) {
-  List<Product> p1 = [
-    Product(cust_id: 101, cust_name: "name", cust_contact: "123456789"),
-  ];
+  customer C1 = customer();
+  C1
+    ..setCustomerdata()
+    ..cart_product();
 }
