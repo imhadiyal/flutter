55c0b95b38480c2c4d91e4a855acf70560705473
Darshan Hadiyal <146731392+imhadiyal@users.noreply.github.com>
2024-04-15 12:55:57 +0530

    2.dart

--- pr-3/2.dart
+++ pr-3/2.dart
@@ -1,136 +1,139 @@
-import 'dart:ffi';
 import 'dart:io';
 
-class customer {
-  int? id;
-  String? name;
-  int? contect;
-  int? cart;
-  int? pid;
-  String? pro_name;
-  int? price;
-  int A = 0;
-  int quantity = 0;
-  int? aumount;
+class Customer {
+  int cust_id;
+  String cust_name;
+  String cust_contact;
+  List<Map<String, dynamic>> cust_cart = [];
 
-  setCustomerdata() {
-    print("\t\t|===========================|");
-    print("\t\t|    🛒 CUSTOMER DATA 🛒    |");
-    print("\t\t|===========================|");
-    stdout.write("\n\n* ENTER YOUR ID = ");
-    id = int.parse(stdin.readLineSync() ?? "0");
-    stdout.write("* ENTER YOUR NAME = ");
-    name = stdin.readLineSync() ?? "name";
-    stdout.write("* ENTER YOUR CONTECT NUMBER = ");
-    contect = int.parse(stdin.readLineSync() ?? "0");
+  Customer(this.cust_id, this.cust_name, this.cust_contact);
+
+  void addToCart(int pro_id, String pro_name, int pro_qty, double pro_price) {
+    cust_cart.add({
+      'pro_id': pro_id,
+      'pro_name': pro_name,
+      'pro_qty': pro_qty,
+      'pro_price': pro_price,
+    });
   }
 
-  cart_product() {
-    do {
-      stdout.write("1.[TV 📺]\n");
-      stdout.write("2.[SCHOOL BAG 🎒]\n");
-      stdout.write("3.[HADEPHONE 🎧]\n");
-      stdout.write("4.[COUMPUTER 🖥️]\n");
-      stdout.write("5.[WATCH ⌚]\n");
-      stdout.write("6.[MOBILE 📱]\n");
-      stdout.write("7.[BILL ]\n");
-      stdout.write("0.[EXIT ❌]\n");
+  double calculateTotal() {
+    double total = 0;
+    for (var item in cust_cart) {
+      total += item['pro_qty'] * item['pro_price'];
+    }
+    return total;
+  }
 
-      stdout.write("* ENTER CHOOSE ONE = ");
-      cart = int.parse(stdin.readLineSync() ?? "0");
-      switch (cart) {
-        case 1:
-          A = 45000;
-          print("\t\t|===========================|");
-          print("\t\t|          📺 TV 📺         |");
-          print("\t\t|===========================|");
-          print("* SERIAL NO. = 100");
-          print("* PRODUCT NAME = TV");
-          print("* PRODUCT PRICE =  $A ");
-          stdout.write("* ENTER QUANTITY = ");
-          quantity = int.parse(stdin.readLineSync() ?? "0");
-          break;
-        case 2:
-          A = 3000;
-          print("\t\t|===========================|");
-          print("\t\t|     🎒 SCHOOL BAG 🎒      |");
-          print("\t\t|===========================|");
-          print("* SERIAL NO. = 200");
-          print("* PRODUCT NAME = SCHOOL BAG ");
-          print("* PRODUCT PRICE =  $A ");
-          stdout.write("* ENTER QUANTITY = ");
-          quantity = int.parse(stdin.readLineSync() ?? "0");
-          break;
-        case 3:
-          A = 25000;
-          print("\t\t|===========================|");
-          print("\t\t|      🎧 HADEPHONE 🎧      |");
-          print("\t\t|===========================|");
-          print("* SERIAL NO. = 300");
-          print("* PRODUCT NAME = HADEPHONE ");
-          print("* PRODUCT PRICE =  $A ");
-          stdout.write("* ENTER QUANTITY = ");
-          quantity = int.parse(stdin.readLineSync() ?? "0");
-          break;
-        case 4:
-          A = 60000;
-          print("\t\t|===========================|");
-          print("\t\t|      🖥️ COUMPUTER 🖥️        |");
-          print("\t\t|===========================|");
-          print("* SERIAL NO. = 400");
-          print("* PRODUCT NAME = COUMPUTER");
-          print("* PRODUCT PRICE =  $A ");
-          stdout.write("* ENTER QUANTITY = ");
-          quantity = int.parse(stdin.readLineSync() ?? "0");
-          break;
+  double calculateDiscount(double total) {
+    double discount = 0;
+    if (total >= 500 && total < 1500) {
+      discount = total * 0.1;
+    } else if (total >= 1500 && total < 3500) {
+      discount = total * 0.2;
+    } else if (total >= 3500 && total < 6500) {
+      discount = total * 0.25;
+    } else if (total >= 6500) {
+      discount = total * 0.3;
+    }
+    return discount;
+  }
 
-        case 5:
-          A = 15000;
-          print("\t\t|===========================|");
-          print("\t\t|        ⌚ WATCH ⌚       |");
-          print("\t\t|===========================|");
-          print("* SERIAL NO. = 500");
-          print("* PRODUCT NAME = WATCH");
-          print("* PRODUCT PRICE =  $A ");
-          stdout.write("* ENTER QUANTITY = ");
-          quantity = int.parse(stdin.readLineSync() ?? "0");
-          break;
-        case 6:
-          A = 150000;
-          print("\t\t|===========================|");
-          print("\t\t|       📱 MOBILE 📱       |");
-          print("\t\t|===========================|");
-          print("* SERIAL NO. = 600");
-          print("* PRODUCT NAME = MOBILE");
-          print("* PRODUCT PRICE =  $A ");
-          stdout.write("* ENTER QUANTITY = ");
-          quantity = int.parse(stdin.readLineSync() ?? "0");
-          break;
-
-        case 7:
-          print("\t\t|===========================|");
-          print("\t\t|       ..YOUR BILL..       |");
-          print("\t\t|===========================|");
-          print("ID =        $id");
-          print("NAME =      $name");
-          print("CONTECT =    $contect");
-          print("AMOUNT =    ${A * quantity}");
-          break;
-
-        case 0:
-          print("\n\n\t\t...EXIT ❌...\n\n");
-          break;
-
-        default:
-          print("\n\n\t\t..❗❗ INVALID INPUT ❗❗..\n\n");
-      }
-    } while (cart != 0);
+  void displayDetails() {
+    print('Customer ID: $cust_id');
+    print('Customer Name: $cust_name');
+    print('Customer Contact: $cust_contact');
+    print('Products Purchased:');
+    for (var item in cust_cart) {
+      print( 'Product ID: ${item['pro_id']}, Name: ${item['pro_name']}, Quantity: ${item['pro_qty']},Price: ${item['pro_price']}');
+    }
+    double total = calculateTotal();
+    double discount = calculateDiscount(total);
+    double finalAmount = total - discount;
+    print('Total Amount: \$${total.toStringAsFixed(2)}');
+    print('Discount Amount: \$${discount.toStringAsFixed(2)}');
+    print('Final Amount after Discount: \$${finalAmount.toStringAsFixed(2)}');
   }
 }
 
-void main(List<String> args) {
-  customer C1 = customer();
-  C1
-    ..setCustomerdata()
-    ..cart_product();
+void main() {
+  List<Customer> customers = [];
+
+  while (true) {
+    print('1. Add Customer');
+    print('2. Add Product to Customer Cart');
+    print('3. Calculate and Display Customer Bill');
+    print('4. Search Customer by ID');
+    print('5. Exit');
+    stdout.write('Enter your choice: ');
+    int choice = int.parse(stdin.readLineSync()!);
+
+    if (choice == 1) {
+      stdout.write('Enter Customer ID: ');
+      int cust_id = int.parse(stdin.readLineSync()!);
+      
+      stdout.write('Enter Customer Name: ');
+      String cust_name = stdin.readLineSync()!;
+      
+      stdout.write('Enter Customer Contact: ');
+      String cust_contact = stdin.readLineSync()!;
+      
+      Customer customer = Customer(cust_id, cust_name, cust_contact);
+      customers.add(customer);
+      
+      print('Customer Added Successfully!');
+     } else if (choice == 2) {
+      
+      stdout.write('Enter Customer ID: ');
+      int cust_id = int.parse(stdin.readLineSync()!);
+      
+      var customer =
+          customers.firstWhere((element) => element.cust_id == cust_id);
+      if (customer != null) {
+        
+        stdout.write('Enter Product ID: ');
+        int pro_id = int.parse(stdin.readLineSync()!);
+        
+        stdout.write('Enter Product Name: ');
+        String pro_name = stdin.readLineSync()!;
+        
+        stdout.write('Enter Product Quantity: ');
+        int pro_qty = int.parse(stdin.readLineSync()!);
+        
+        stdout.write('Enter Product Price: ');
+        double pro_price = double.parse(stdin.readLineSync()!);
+        
+        customer.addToCart(pro_id, pro_name, pro_qty, pro_price);
+        print('Product Added to Cart Successfully!');
+      } else {
+        print('Customer not found!');
+      }
+    } else if (choice == 3) {
+      stdout.write('Enter Customer ID: ');
+      int cust_id = int.parse(stdin.readLineSync()!);
+      var customer =
+          customers.firstWhere((element) => element.cust_id == cust_id);
+      if (customer != null) {
+        customer.displayDetails();
+      } else {
+        print('Customer not found!');
+      }
+    } else if (choice == 4) {
+      stdout.write('Enter Customer ID to search: ');
+      int cust_id = int.parse(stdin.readLineSync()!);
+      
+      var customer = customers.firstWhere((element) => element.cust_id == cust_id);
+      if (customer != null) {
+        print('Customer Found:');
+        customer.displayDetails();
+      } else {
+        print('Customer not found!');
+      }
+    } else if (choice == 5) {
+      print('Exiting...');
+      break;
+    } else {
+      print('Invalid choice! Please try again.');
+    }
+  }
 }
